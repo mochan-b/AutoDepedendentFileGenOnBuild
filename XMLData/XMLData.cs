710fafa6a94d66205dd436c9685601d81a3fbc7e
Mochan Shrestha <mochan.shrestha@gmail.com>
2017-06-05 01:39:42 -0400

    Full working project and the first draft of the README post.

---
+++ XMLData/XMLData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLData
+{
+    /// <summary>
+    /// Contains the data that we will use to create the XML file
+    /// </summary>    
+    public class XMLData
+    {
+        /// <summary>
+        /// Class that holds the salary information
+        /// </summary>
+        [Serializable]
+        public class SalaryData
+        {
+            [XmlAttribute]
+            public string FirstName, LastName;
+            [XmlAttribute("Salary")]
+            public int Salary;
+
+            public override string ToString()
+            {
+                return FirstName + " " + LastName + " \t\t: $" + Salary + ",000";
+            }
+        }
+
+        // Data that we will write to the XML file
+        List<SalaryData> data = new List<SalaryData>();
+
+        /// <summary>
+        /// Read the input file that is comma separated data and output as XML file
+        /// </summary>
+        /// <param name="inputfile">Name of input file</param>
+        public XMLData(string inputfile)
+        {
+            // Read the comma separated data of 5 items and split them by the names
+            var lines = System.IO.File.ReadAllLines("input.txt");
+            foreach (var line in lines)
+            {
+                var splits = line.Split(new char[] { ',' });
+                data.Add(new SalaryData() { FirstName = splits[0], LastName = splits[1], Salary = Convert.ToInt32(splits[2]) });
+            }
+
+            // Save the file as data.xml
+            var writer = new StreamWriter("data.xml");
+            var serializer = new XmlSerializer(data.GetType());
+            serializer.Serialize(writer, data);
+            writer.Close();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new XMLData("");
+        }
+    }
+}
---
+++ XMLMain/XMLMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+using static XMLData.XMLData;
+
+namespace XMLMain
+{
+    class XMLMain
+    {
+        static void Main(string[] args)
+        {
+            // Salary data
+            List<SalaryData> data = new List<SalaryData>();
+
+            // Read the XML data and output the data
+            var reader = new StreamReader("data.xml");
+            var serializer = new XmlSerializer(data.GetType());
+            data = serializer.Deserialize(reader) as List<SalaryData>;
+            reader.Close();
+            foreach (var datum in data)
+                Console.WriteLine(datum);
+        }
+    }
+}
